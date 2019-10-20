50d699eba5c5574ad84ed0689d021cb63b9f9fd7
Pavel <49363068+Zalivator@users.noreply.github.com>
2019-10-20 21:53:38 +0300

    Add projects

---
+++ projects/repair-design-project/desktop.js
@@ -0,0 +1,11 @@
+const back__button = document.getElementById('back');
+const mobile__button = document.getElementById('mobile');
+back__button.style.display = 'block';
+back__button.onclick = () => {
+    window.location.href = '../../index.html';
+};
+
+mobile__button.onclick = () => {
+    window.location.href = './mobile_index.html';
+
+};
